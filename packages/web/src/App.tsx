27828c82733ee3e0b7c270a2ac9536058ebebbb9
Leonardo Dias Lissone Santomero <leonardo.lissonez@gmail.com>
2021-10-25 17:14:46 -0300

    send message form styled

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,14 +1,21 @@
-import { LoginBox } from './components/LoginBox'
+import { useContext } from 'react'
+
 import { MessageList } from './components/MessageList'
+import { SendMessageForm } from './components/SendMessageForm'
+import { LoginBox } from './components/LoginBox'
+
+import { AuthContext } from './context/auth'
 
 import styles from './styles/app.module.scss'
 
 export function App() {
+  const { user } = useContext(AuthContext)
+
   return (
     <main className={styles.contentWrapper}>
       <MessageList />
 
-      <LoginBox />
+      { !!user ? <SendMessageForm /> : <LoginBox />}
     </main>
   )
 }
